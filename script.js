26761a2a5972191162b3cc1de819405f33d8261a
PatrickB <84555143+Goodluckboey@users.noreply.github.com>
2021-11-25 15:28:37 +0800

    updated Number of Monsters

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let heroPosX = 0 //limit = 688
 let heroPosY = 0 //limit = 344
 let directionChecker = 'right'
-let NUMBER_OF_MONSTERS = 24
+let NUMBER_OF_MONSTERS = 23
 let heroRange = 65
 let heroStats = {
   maxHealth: 100,
